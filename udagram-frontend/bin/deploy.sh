36bcd48ade6102af594208661eec4b645f3211bd
Mahmoud Ahmad <mahmoudcyyki@gmail.com>
2022-03-25 22:13:49 +0200

    Fix issues

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 --region us-east-1f cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-079290547713/
+aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-079290547713/
