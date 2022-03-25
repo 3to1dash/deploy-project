5de66735ca45c6571f5d7c39e2a5111446c4e63d
Mahmoud Ahmad <mahmoudcyyki@gmail.com>
2022-03-25 23:05:20 +0200

    Fix s3 script

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read-write ./www s3://elasticbeanstalk-us-east-1-079290547713/ --region us-east-1 --endpoint-url s3-website-us-east-1.amazonaws.com
+aws s3 cp --recursive --acl public-read-write ./www/ s3://elasticbeanstalk-us-east-1-079290547713/ --region us-east-1
