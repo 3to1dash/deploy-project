8fc04af84481d91481f4b854c74cc3572a63412a
Mahmoud Ahmad <mahmoudcyyki@gmail.com>
2022-03-25 22:37:36 +0200

    Change S3 Access control list to public read and write

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-079290547713 --debug
+aws s3 cp --recursive --acl public-read-write ./www s3://elasticbeanstalk-us-east-1-079290547713 --debug
